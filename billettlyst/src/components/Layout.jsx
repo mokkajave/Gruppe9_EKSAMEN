0f9688fe362cd186d1afa1dc498b93b61a4ec9ae
jave <111729472+mokkajave@users.noreply.github.com>
2025-04-30 17:19:40 +0200

    Implementert responsivt design ++
    Gjort nettsiden responsiv - laget en container for å begrense alt innhold i header, main og footer til 1200 pixler (på større skjemer).
    
    Ferdigstilt navigasjonsbar ifht design og plassering.
    
    Lagt til retningslinjer i global.scss, samt fargevariabler ++.

--- billettlyst/src/components/Layout.jsx
+++ billettlyst/src/components/Layout.jsx
@@ -5,14 +5,20 @@ import "../styles/layout.scss";
 export default function Layout({children}) {
     return (
         <>
-            <header className="grid">
-                <Nav />
+            <header>
+                <div className="content-container">
+                    <Nav />
+                </div>
             </header>
             <main>
-                {children}
+                <div className="content-container">
+                    {children}
+                </div>
             </main>
             <footer>
-
+                <div className="content-container">
+                    <p>Website &copy; 2025 </p>
+                </div>
             </footer>
         </>
     )
--- billettlyst/src/components/Nav.jsx
+++ billettlyst/src/components/Nav.jsx
@@ -5,14 +5,14 @@ import "../styles/nav.scss";
 export default function Nav() {
     return (
         <nav>
-            <Link to="/" id="logo">BilletLyst</Link>
+            <Link to="/" id="logo">BillettLyst</Link>
             <ul>
-                <li><Link to="/">Hjem</Link></li>
-                <li><Link to="#">Musikk</Link></li>
-                <li><Link to="#">Sport</Link></li>
-                <li><Link to="#">Teater</Link></li>
+                <li><Link to="/" className="nav-link">Hjem</Link></li>
+                <li><Link to="#" className="nav-link">Musikk</Link></li>
+                <li><Link to="#" className="nav-link">Sport</Link></li>
+                <li><Link to="#" className="nav-link">Teater</Link></li>
             </ul>
-            <Link to="/dashboard">Logg inn</Link>
+            <Link to="/dashboard" className="nav-link nav-link-button">Logg inn</Link>
         </nav>
     )
 }
