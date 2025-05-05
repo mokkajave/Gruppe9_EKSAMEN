3754d9e3ed9d6ef6ff33af3328b9c1fa3f1b2d25
jave <111729472+mokkajave@users.noreply.github.com>
2025-05-05 19:58:30 +0200

    Lagt til overskrift på kategorisidene
    Benyttet slug til å lage overskrift per kategoriside (CategoryPage.jsx).

--- billettlyst/src/pages/CategoryPage.jsx
+++ billettlyst/src/pages/CategoryPage.jsx
@@ -1,7 +1,21 @@
+import { useParams } from "react-router-dom";
+
+import "../styles/categoryPage.scss";
+
 export default function CategoryPage() {
+    const {slug} = useParams()
+
+    const backgroundImages = {
+        musikk: "#",
+        sport: "#",
+        teater: "#"
+    }
+
+    const backgroundImage = backgroundImages[slug];
+
     return (
-        <section>
-            <h1>Category</h1>
+        <section className="category-hero">
+            <h1>{slug.charAt(0).toUpperCase() + slug.slice(1)}</h1>
         </section>
     )
 }
